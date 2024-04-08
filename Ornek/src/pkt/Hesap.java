f6a60145248c9ffa0dd876086e2620c3a77b3aad
ozkanberkee <67657051+ozkanberkee@users.noreply.github.com>
2024-04-08 04:16:18 +0300

    Update Hesap.java

--- Ornek/src/pkt/Hesap.java
+++ Ornek/src/pkt/Hesap.java
@@ -10,16 +10,44 @@ import java.util.UUID;
 public class Hesap implements IHesap {
 	private String no;
 	private double bakiye;
+	
 	public Hesap()
 	{
 		this.no = UUID.randomUUID().toString(); // Rastgele bir Id belirleniyor...
 		// Hesap açılışı bakiye sıfır.
 		bakiye = 0;
 	}
+	
 	@Override
 	public String getNo() {
 		return no;
 	}
+
+	@Override
+	public boolean paraCek(double miktar) {
+		/*
+		 * Ön miktar kontrol ediliyor.
+		 */
+		if(miktar <= 0 || miktar > bakiye) return false;
+		bakiye -= miktar;
+		return true;
+	}
+
+	@Override
+	public boolean paraYatir(double miktar) {
+		if(miktar <= 0) return false; // miktar kontrolü
+		bakiye += miktar;
+		return true;
+	}
+
+	@Override
+	public double getBakiye() {
+		return bakiye;
+	}
 	
+	@Override
+	public String toString() {
+		return "Hesap No: " + no + "\nBakiye: " + bakiye;
+	}
 
 }
