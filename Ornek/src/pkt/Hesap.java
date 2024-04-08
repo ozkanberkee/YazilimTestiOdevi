1606ec0c05a4fa29daf68a3914684bbcca607f00
ozkanberkee <67657051+ozkanberkee@users.noreply.github.com>
2024-04-08 03:35:18 +0300

    Update Hesap.java

--- Ornek/src/pkt/Hesap.java
+++ Ornek/src/pkt/Hesap.java
@@ -17,37 +17,5 @@ public class Hesap implements IHesap {
 		// Hesap açılışı bakiye sıfır.
 		bakiye = 0;
 	}
-	
-	@Override
-	public String getNo() {
-		return no;
-	}
-
-	@Override
-	public boolean paraCek(double miktar) {
-		/*
-		 * Ön miktar kontrol ediliyor.
-		 */
-		if(miktar <= 0 || miktar > bakiye) return false;
-		bakiye -= miktar;
-		return true;
-	}
-
-	@Override
-	public boolean paraYatir(double miktar) {
-		if(miktar <= 0) return false; // miktar kontrolü
-		bakiye += miktar;
-		return true;
-	}
-
-	@Override
-	public double getBakiye() {
-		return bakiye;
-	}
-	
-	@Override
-	public String toString() {
-		return "Hesap No: " + no + "\nBakiye: " + bakiye;
-	}
 
 }
