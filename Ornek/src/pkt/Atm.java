0db6bc7fdd25f55f4d5df9a171060d84543e80b2
ozkanberkee <67657051+ozkanberkee@users.noreply.github.com>
2024-04-08 04:24:39 +0300

    Update Atm.java

--- Ornek/src/pkt/Atm.java
+++ Ornek/src/pkt/Atm.java
@@ -21,7 +21,7 @@ public class Atm {
 	}
 	
 	public boolean paraYatir(IKart kart,String sifre, double miktar) {
-		// Şifre kontrolü
+		
 		if(!kart.girisKontrol(sifre)) return false;
 		return kart.getHesap().paraYatir(miktar);
 	}
