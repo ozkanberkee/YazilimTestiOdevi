d3615f72cb9e0c6ad4dcabfc9a047f6ebc70a998
ozkanberkee <67657051+ozkanberkee@users.noreply.github.com>
2024-04-08 04:08:02 +0300

    Update Hesap.java

--- Ornek/src/pkt/Hesap.java
+++ Ornek/src/pkt/Hesap.java
@@ -10,7 +10,12 @@ import java.util.UUID;
 public class Hesap implements IHesap {
 	private String no;
 	private double bakiye;
-	
+	public Hesap()
+	{
+		this.no = UUID.randomUUID().toString(); // Rastgele bir Id belirleniyor...
+		// Hesap açılışı bakiye sıfır.
+		bakiye = 0;
+	}
 	
 	
 
